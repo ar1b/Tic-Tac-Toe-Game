41d7721fa36cdeebe88f09529949140c92982949
ar1b <arib.kamal99@gmail.com>
2026-03-03 16:36:00 -0500

    Fixed turn leakage, rematch functionality, memeory reset when starting a new game

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,18 @@ import React, {useState} from 'react';
 import Player from "./components/Player"
 import Gameboard from "./components/Gameboard"
 import Log from './components/Log';
+import GameOver from './components/GameOver';
+import { WINNING_COMBINATIONS } from './winning-combinations';
 
+
+//Initial gameboard, computeed value
+const initalGameboard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+]
+
+//helper function to determine active player
 const deriveActivePlayer = (gameTurns) => {
   let currentPlayer = 'X';
 
@@ -16,12 +27,33 @@ const deriveActivePlayer = (gameTurns) => {
 function App() {
 
   const [gameTurns, setGameTurns] = useState([]);
-  //const [activePlayer, setActivePlayer] = useState('X');
-
   const activePlayer = deriveActivePlayer(gameTurns);
 
+  let gameBoard = [...initalGameboard.map(array => [...array])];
+
+  for(const turn of gameTurns) {
+    const {square, player} = turn
+    const {row, column} = square
+
+    gameBoard[row][column] = player
+  }
+
+  let winner = null
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column]
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column]
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column]
+
+    if(firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol) {
+      winner = firstSquareSymbol
+    }
+
+  }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
+
   const handleSelectSquare = (rowIndex, columnIndex) => {
-    //setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X')
     setGameTurns((prevTurns) => {
       
       const currentPlayer = deriveActivePlayer(prevTurns)
@@ -31,6 +63,11 @@ function App() {
       return updatedTurns
     })
   }
+
+  const handleRestart = () => {
+    setGameTurns([])
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -38,7 +75,8 @@ function App() {
           <Player initalName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
           <Player initalName="Player 2" symbol="Y"  isActive={activePlayer === 'O'}/>
         </ol>
-        <Gameboard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} turns={gameTurns}/> 
+        {( winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
+        <Gameboard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} board={gameBoard}/> 
       </div>
       <Log turns={gameTurns}/>
     </main>
--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,26 +1,12 @@
-const initalGameboard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-]
+const Gameboard = ({onSelectSquare, board}) => {
 
-const Gameboard = ({onSelectSquare, turns}) => {
-
-  let gameBoard = initalGameboard;
-
-  for(const turn of turns) {
-    const {square, player} = turn
-    const {row, column} = square
-
-    gameBoard[row][column] = player
-  }
 
   return (
     <ol id='game-board'>
-        {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
+        {board.map((row, rowIndex) => <li key={rowIndex}>
             <ol>
                 {row.map((playerSymbol, columnIndex) => <li key={columnIndex}>
-                    <button onClick={() => onSelectSquare(rowIndex, columnIndex)}>{playerSymbol}</button>
+                    <button onClick={() => onSelectSquare(rowIndex, columnIndex)} disabled={playerSymbol !== null}>{playerSymbol}</button>
                 </li>)}
             </ol>
         </li>)}
